b7db9c8b8583e2c8d6a9cd8473b435e3848c5592
Prasad Jayasundara <prasad.j@eyepax.com>
2019-05-17 17:52:19 +0530

    table

--- admin/admin_update.php
+++ admin/admin_update.php
@@ -10,17 +10,67 @@
 <body>
 <form class="form-inline">
   <label class="sr-only" for="inlineFormInput">Name</label>
-  <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" placeholder="Jane Doe">
+  <input type="text" class="form-control mb-2 mr-sm-2 mb-sm-0" id="inlineFormInput" name="book_id" placeholder="Book ID">
 
   <label class="sr-only" for="inlineFormInputGroup">Username</label>
   <div class="input-group mb-2 mr-sm-2 mb-sm-0">
-    <div class="input-group-addon">@</div>
-    <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Username">
+    <div class="input-group-addon">Or</div>
+    <input type="text" class="form-control" id="inlineFormInputGroup" name="book_name" placeholder="Book Name">
   </div>
 
 
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Search</button>
 </form>
+
+<?php 
+   include_once 'dbconnect.php';
+
+   if(isset($_POST['submit'])){
+
+        $B_NAME = $_POST['book_name'];
+        $B_ID = $_POST['book_id'];
+
+        $sql = "Select * from books_details where book_id='$B_ID' and book_name='$B_ID';";
+        $result = mysqli_query($conn,$sql);
+        $resultCheck = mysqli_num_rows($result);
     
+        if($resultCheck > 0) {
+             while ($row = mysqli_fetch_assoc($result)){
+                 $ID = $row['book_id'];
+                 $NAME = $row['book_name'];
+                 $PRICE = $row['price'];
+                //echo $row['username'];
+                
+                <table class="table">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">ID</th>
+                    <th scope="col">NAME</th>
+                    <th scope="col">PRICE</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>>echo $row['book_id']</td>
+                    <td>>echo $row['book_id']</td>
+                    <td>>echo $row['book_id']</td>
+                    </tr>
+                    
+                </tbody>
+            </table>
+            
+                echo $NAME;
+            }
+        }
+
+    }
+?>
+    
+
+   
+
 </body>
 </html>
+
